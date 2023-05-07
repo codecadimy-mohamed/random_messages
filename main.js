e51ace246060e5e9d799d802573881b52b362cc4
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-05-07 16:20:11 +0200

    store messages data

---
+++ main.js
@@ -0,0 +1,2 @@
+console.log('Hello, World!');
+const phrases = [['Believe in yourself, ',["don't give up.","and your abilities.", "keep pushing forward.", "you are capable.","dream big and go for it!"]],["Success: ",["Don't give up.","Believe in yourself.","Taking action.","Mistakes.","Dream big"]]];

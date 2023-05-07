25aba7402cb500991cb47edb07c0e064579ab321
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-05-07 16:43:59 +0200

    make function that does random messages

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 console.log('Hello, World!');
-const phrases = [['Believe in yourself, ',["don't give up.","and your abilities.", "keep pushing forward.", "you are capable.","dream big and go for it!"]],["Success: ",["Don't give up.","Believe in yourself.","Taking action.","Mistakes.","Dream big"]]];
+const phrases = [['Believe in yourself, ', ["don't give up.", "and your abilities.", "keep pushing forward.", "you are capable.", "dream big and go for it!"]], ["Success: ", ["Don't give up.", "Believe in yourself.", "Taking action.", "Mistakes.", "Dream big"]]];
+const randomMessages = () => {
+  const firstNumbers = Math.floor(Math.random() * 2);
+  const secondNumber = Math.floor(Math.random() * 5);
+  // console.log(phrases[firstNumbers][2][secondNumber]);
+  console.log(`${phrases[firstNumbers][0]}${phrases[firstNumbers][1][secondNumber]}`);
+};
+randomMessages();
